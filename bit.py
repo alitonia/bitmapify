44d5b1db36fd7727e888bbe9ddec818dd9b32dd4
Zen-net <allyofjustice1@gmail.com>
2020-06-04 15:06:30 +0700

    Generalize bit format Explicit comment

--- bit.py
+++ bit.py
@@ -5,28 +5,36 @@ from PIL import Image
 
 
 def main():
-	bit_size = 16
-	shape = int(256 / bit_size)
-	link = '/home/zen-net/Downloads/pine_apple.png'
+	bit_size_x = 32
+	bit_size_y = 32
+	shape_x = 256
+	shape_y = 256
+
+	size_x = int(shape_x / bit_size_x)
+	size_y = int(shape_y / bit_size_y)
+
+	link = '/home/user/pictures/pine_apple.png'
 	try:
 		img = Image.open(link)
 		img = img.convert(mode='RGB')
-		img = img.resize((shape, shape), Image.ANTIALIAS)
+		img.show()
+		img = img.resize((size_x, size_y), Image.ANTIALIAS)
 		#
 		pix = numpy.array(img.getdata())
 		# print(pix[:10])
 		# img = img.resize((1024, 1024), Image.ANTIALIAS)
 
-		# img.show()
-
 		s = pix
-		x = '.eqv MONITOR_SCREEN 0x10010000\n\n.text\nli $k0, MONITOR_SCREEN\n\n# Shape 256x256, pixel 16x16\n\n'
+		x = '.eqv MONITOR_SCREEN 0x10010000\n\n.text\nli $k0, MONITOR_SCREEN\n\n'
+		x += (
+				'# shape ' + str(shape_x) + 'x' + str(shape_y)
+				+ ', pixel ' + str(bit_size_x) + 'x' + str(bit_size_y) + '\n'
+				+ '# from ' + str(re.search(r'/[\w.-]+$', link).group(0))[1:] + '\n\n'
+		)
 		t = 0
 		for color in s:
 			r, g, b = color
-			print(r, g, b)
 			my_color = '0x' + str(hex(int(r)))[2:] + str(hex(int(g)))[2:] + str(hex(int(b)))[2:]
-			#
 			x += ('li $t0, ' + my_color + '\nsw $t0, ' + str(t) + '($k0)\n nop\n')
 			t += 4
 		print(x.count('\n'))
